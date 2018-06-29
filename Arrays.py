8bf66b6cc8a5409f58e86c58c95cb94f30d40eff
Jailson Soares <38081625+jailsonsf@users.noreply.github.com>
2018-06-29 20:44:41 -0300

    Arrays

---
+++ Arrays.py
@@ -0,0 +1,38 @@
+tam_A, tam_B = map(int, input().split(' '))
+k, m = map(int, input().split(' '))
+A = [int(x) for x in input().split(' ')]
+B = [int(x) for x in input().split(' ')]
+
+A = sorted(A)
+B = sorted(B)
+
+sub_A = []
+sub_B = []
+
+menores = 0
+
+for i in range(0,k):
+
+    sub_A.append(A[i])
+    
+for i in range(0,m):
+
+    sub_B.append(B[i])
+
+
+for i in sub_A:
+
+    for x in sub_B:
+    
+        if i < x:
+            
+            menores += 1
+
+
+if(menores == (k * m)):
+
+    print('YES')
+
+else:
+
+    print('NO')
